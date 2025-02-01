66de2efe1e2af07d5a7f2a77a2df99018cb1feaf
alexeenkoivan <alexeenkoivan@gmail.com>
2025-02-01 15:12:03 +0300

    Step 11.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import PropTypes from 'prop-types';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider, ErrorBoundary } from '@rollbar/react';
+import rollbarInstance from './config/rollbar';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -20,34 +22,38 @@ PrivateRoute.propTypes = {
 
 function App() {
   return (
-    <SocketProvider>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </Router>
-    </SocketProvider>
+    <Provider instance={rollbarInstance}>
+      <ErrorBoundary>
+        <SocketProvider>
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <HomePage />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+          </Router>
+        </SocketProvider>
+      </ErrorBoundary>
+    </Provider>
   );
 }
 
