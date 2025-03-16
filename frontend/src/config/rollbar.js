a82c104a1136722d2956b0988aa36bf89581aac7
alexeenkoivan <alexeenkoivan@gmail.com>
2025-03-16 20:59:32 +0300

    Review 1.7

--- frontend/src/config/rollbar.js
+++ frontend/src/config/rollbar.js
@@ -1,16 +1,12 @@
 import Rollbar from 'rollbar';
 
-const rollbarToken = process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN;
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: process.env.NODE_ENV || 'production',
+};
 
-const rollbarConfig = rollbarToken
-  ? {
-      accessToken: rollbarToken,
-      captureUncaught: true,
-      captureUnhandledRejections: true,
-      environment: process.env.NODE_ENV || 'production',
-    }
-  : null;
-
-const rollbarInstance = rollbarConfig ? new Rollbar(rollbarConfig) : null;
+const rollbarInstance = new Rollbar(rollbarConfig);
 
 export default rollbarInstance;
