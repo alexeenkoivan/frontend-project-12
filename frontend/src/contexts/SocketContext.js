e48cfcbec476a120e1264487010d2c13a241c809
alexeenkoivan <alexeenkoivan@gmail.com>
2025-03-16 20:06:00 +0300

    Review 1.4

--- frontend/src/contexts/SocketContext.js
+++ frontend/src/contexts/SocketContext.js
@@ -10,7 +10,7 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io('/', { withCredentials: true });
+    const newSocket = io('http://localhost:5000', { withCredentials: true });
     setSocket(newSocket);
 
     newSocket.on('connect', () => {
