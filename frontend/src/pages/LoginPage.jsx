6f46deb39e826504be7467c0881e0dcf72e2de4b
alexeenkoivan <alexeenkoivan@gmail.com>
2025-03-20 16:14:05 +0300

    Review 1.21

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -89,7 +89,9 @@ const LoginPage = () => {
               </div>
               <div className="card-footer p-4">
                 <div className="text-center">
-                  <span>{t('login.newToChat')}</span> <a href="/signup">{t('login.signup')}
+                  <span>{t('login.newToChat')}</span>
+                  <a href="/signup">
+                    {t('login.signup')}
                   </a>
                 </div>
               </div>
